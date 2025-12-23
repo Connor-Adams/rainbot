4fe160b23ef799ac12298827ee7dceadda24389d
Connor Adams <ceeman.adams@gmail.com>
2025-12-23 12:57:43 -0500

    Add Husky pre-commit hooks and Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'commands/**/*.ts',
+    'utils/**/*.ts',
+    'server/**/*.ts',
+    'handlers/**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  verbose: true,
+};
